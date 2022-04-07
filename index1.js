e1d7eb47df9751a08ae365dfb1e62472d7ba5761
mohammadali0429 <mohammadalidodiya121@gmail.com>
2022-04-07 13:52:47 +0530

    first commit

---
+++ index1.js
@@ -0,0 +1,24 @@
+// const http = require('http');
+
+// http.createServer((req,resp)=>{
+//     resp.write("<h1>Hello  is Dodiya Mohammad</h1>");
+//     resp.end();
+// }).listen(4500);
+
+// var http = require('http');
+
+//create a server object:
+// http.createServer(function (req, res) {
+//   res.write('Hello!'); //write a response to the client
+//   res.end(); //end the response
+// }).listen(8080); //the server object listens on port 8080
+
+// athva practice mate
+
+
+// const http = require('http');
+
+// http.createServer((req,resp)=>{
+//     resp.write("<h1>Hello,This is Dodiya Mohammad</h1>");
+//     resp.end();
+// }).listen(9090);
